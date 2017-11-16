c1cf45531c379d842747b531144fcc827ebeedbe
Thorbjörn Backlund <thorbjorn@spiro.se>
2017-11-16 13:11:24 +0100

    Test creating tks

---
+++ test_create.php
@@ -0,0 +1,36 @@
+<?php
+
+	date_default_timezone_set(@date_default_timezone_get());
+
+	require_once('Tk/Rule.php');
+	require_once('Tk.php');
+
+	use \Spiro\Puggan\Bankgiro\Tk;
+
+	$tks = [];
+
+	$tk = new Tk(51, ['Clearingnummer' => 9900, 'Bankgironummer' => 9912346]);
+	$tks[] = $tk;
+
+	$tk = new Tk(52);
+	$tk->Bankgironummer = 9912346;
+	$tk->Betalarnummer = 10133;
+	$tk->Bankkontonummer = 9918000000041014;
+	$tk->Personnummer = 194512121212;
+	$tks[] = $tk;
+
+	$tk = new Tk(53);
+	$tk['Information'] = "JAG VILL BETALA MÅNADSVIS";
+	$tks[] = $tk;
+
+	$tk = new Tk(54, ['DORIS DEMOSSON', 'C/o DAVID DEMOSSON']);
+	$tks[] = $tk;
+
+	$tks[] = new Tk(55, ['DEMOVÄGEN 1']);
+	$tks[] = new Tk(56, [10000, 'DEMOSTAD']);
+
+	$tks[] = new Tk(59, [NULL, 9900, count($tks) - 1]);
+
+	print_r($tks);
+
+	echo implode(PHP_EOL, $tks) . PHP_EOL;
