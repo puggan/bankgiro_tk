87584b718fb065ffccc6eed3303367568381387f
Thorbjörn Backlund <thorbjorn@spiro.se>
2017-11-16 14:27:47 +0100

    add a load all file

---
+++ autoload.php
@@ -0,0 +1,11 @@
+<?php
+require_once "./Tk/Rule.php";
+require_once "./Tk.php";
+require_once "./Tk52.php";
+require_once "./Tk53.php";
+require_once "./Tk51.php";
+require_once "./Tk54.php";
+require_once "./Tk55.php";
+require_once "./Tk56.php";
+require_once "./Tk59.php";
+require_once "./File/TkFile5x.php";
